5d33809329e5d476a0f02432a439e113041cf0c5
Nikhil Mhatre <nikhilmhatre703@gmail.com>
2024-02-08 16:22:58 +0530

    added auth page

---
+++ lib/types.ts
@@ -0,0 +1,4 @@
+type FieldName = "email" | "username" | "password"; // Union type for field names
+type FieldLabel = string; // String type for field labels
+
+
---
+++ lib/zodSchema.ts
@@ -0,0 +1,41 @@
+"use client"
+
+
+import { z } from "zod"
+
+const SignupFormSchema = z.object({
+    username: z
+        .string()
+        .min(1, { message: "Required Username!" })
+        .max(10),
+    email: z
+        .string()
+        .min(1, { message: "Required Email!" })
+        .email("Invalid Email!"),
+    password: z
+        .string()
+        .min(8, 'Password must be at least 8 characters long')
+        .refine((val) => val.search(/[a-z]/) !== -1, 'Password must contain at least one lowercase character')
+        .refine((val) => val.search(/[A-Z]/) !== -1, 'Password must contain at least one uppercase character')
+        .refine((val) => val.search(/[!@#$%^&*]/) !== -1, 'Password must contain at least one special character')
+        .refine((val) => val.search(/\d/) !== -1, 'Password must contain at least one number')
+})
+
+const SigninFormSchema = z.object({
+    email: z
+        .string()
+        .min(1, { message: "Required Email!" })
+        .email("Invalid Email!"),
+    password: z
+        .string()
+        .min(8, 'Password must be at least 8 characters long')
+        .refine((val) => val.search(/[a-z]/) !== -1, 'Password must contain at least one lowercase character')
+        .refine((val) => val.search(/[A-Z]/) !== -1, 'Password must contain at least one uppercase character')
+        .refine((val) => val.search(/[!@#$%^&*]/) !== -1, 'Password must contain at least one special character')
+        .refine((val) => val.search(/\d/) !== -1, 'Password must contain at least one number')
+})
+
+
+
+
+export { SigninFormSchema, SignupFormSchema, z }
